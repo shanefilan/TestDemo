8975f311be1739750c9726901d241cb351bdacf6
yizhiya <865307149@qq.com>
2017-05-11 10:23:49 +0800

    hello

--- app/src/main/java/com/example/y/testdemo/MainActivity.java
+++ app/src/main/java/com/example/y/testdemo/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //易志亚第一次提交
         //胡鹏程第一次更改
     }
 }
